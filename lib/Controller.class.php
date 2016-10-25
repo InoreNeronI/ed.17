6b8a90b061456183ace17d8d3de06496e0cb6f82
Martin Mozos <martinmozos@gmail.com>
2016-10-26 00:01:15 +0200

    Conexion a bdd y mapeado de campos del login.

--- lib/Controller.class.php
+++ lib/Controller.class.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use App\View;
+use App\Repository;
 use Symfony\Component\HttpFoundation;
 
 /**
@@ -20,16 +20,37 @@ class Controller
 	 */
 	public static function action(HttpFoundation\Request $request)
 	{
+		if ($request->getMethod() === 'POST')
+			static::postActionHook($request);
+
 		/** @var string $slug */
 		$slug = $request->get('_route');
+
 		/** @var array $parameters */
 		$parameters = $request->get('parameters');
+
 		/** @var HttpFoundation\Response $response */
 		$response = new HttpFoundation\Response(View::render($slug, $parameters), 200);
 		$response->headers->set('Content-Type', 'text/html');
 		// configure the HTTP cache headers
-//		$response->setMaxAge(10);
+		//$response->setMaxAge(10);
+
 		// return response object back
 		return $response;
 	}
+
+	/**
+	 * @param HttpFoundation\Request $request
+	 *
+	 * @return HttpFoundation\Response
+	 */
+	public static function postActionHook(HttpFoundation\Request $request)
+	{
+		$postData = $request->request->all();
+		$manager = new Repository;
+		$manager->checkCredentials($postData);
+		/*ladybug_dump($postData);
+		ladybug_dump($manager);*/
+	}
+
 }
---
+++ lib/Repository.class.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App;
+
+use medoo;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class Repository
+ * @package App
+ */
+class Repository extends medoo
+{
+	/** @var array $parameters */
+	private $parameters;
+
+	/**
+	 * Repository constructor.
+	 */
+	public function __construct()
+	{
+		$this->parameters = static::parseConfig();
+		return parent::__construct( array(
+			'database_type' => $this->parameters['database_type'],
+			'database_name' => $this->parameters['database_name'],
+			'server' => $this->parameters['database_host'],
+			'port' => $this->parameters['database_port'],
+			'username' => $this->parameters['database_user'],
+			'password' => $this->parameters['database_password'],
+			'charset' => $this->parameters['database_charset']
+		) );
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getParameters()
+	{
+		return $this->parameters;
+	}
+
+	/**
+	 * @param string $slug
+	 *
+	 * @return array
+	 */
+	private static function parseConfig($slug = 'parameters')
+	{
+		$config = Yaml::parse(file_get_contents(__DIR__."/../app/config/$slug.yml"));
+		return $slug === 'parameters' ? $config[$slug] : $config;
+	}
+
+	/**
+	 * @param string $slug
+	 *
+	 * @return array
+	 */
+	private static function parseDBFields($slug = 'login')
+	{
+		return static::parseConfig("field/$slug");
+	}
+
+	/**
+	 * @param $tables
+	 * @param string $prefix
+	 * @param string $break_table
+	 *
+	 * @return array
+	 */
+	private static function mergeDBFields($tables, $prefix = '', $break_table = 'edg020_ikasleak')
+	{
+		$db_fields = array();
+		foreach ($tables as $table_name => $table_field) {
+			$length = strlen($table_name);
+			$name = $prefix . $table_name;
+			if (strrpos($table_name, '_') === --$length )
+				$db_fields = array_merge( $db_fields, static::mergeDBFields($table_field, $name, $break_table) );
+			else
+				$db_fields[$name] = $table_field;
+			if ($table_name === $break_table)
+				return $db_fields;
+		}
+		return $db_fields;
+	}
+
+	/**
+	 * @param $form_fields
+	 * @param string $slug
+	 * @param string $table
+	 *
+	 * @return array
+	 */
+	public static function checkCredentials($form_fields, $slug = 'login', $table = 'edg020_ikasleak')
+	{
+		$db_fields = array();
+		$config = static::parseDBFields($slug);
+		foreach ($form_fields as $form_field_name => $form_field_value) {
+			if (array_key_exists($form_field_name, $config))
+				$db_fields[$form_field_name] = static::mergeDBFields($config[$form_field_name], $table);
+		}
+		//print_r($db_fields);
+	}
+}
