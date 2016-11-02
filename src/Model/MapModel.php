1835fe738ebe8b3a9af91a9719417a1c221df3e8
Martin Mozos <martinmozos@gmail.com>
2016-11-02 16:16:48 +0100

    Pruebas del componente DBAL.

--- src/Model/MapModel.php
+++ src/Model/MapModel.php
@@ -28,9 +28,35 @@ final class MapModel extends Model
      */
     public function checkCredentials(array $fields, $slug = LOGIN_SLUG, array $params = PARAMETERS, $table = USER_TABLE)
     {
-        parent::__construct($params);
+        parent::__construct(null, null, null, null, null, $params);
+        $periodo = $fields['fperiodo'];
+        $cod_alumno = $fields['fcodalued'];
+        $naci_dia_alumno = $fields['ffnacidia'];
+        $naci_mes_alumno = $fields['ffnacimes'];
+        $cod_prueba = $fields['fcodprueba'];
+
+        dump($fields['fperiodo']);
+        //dump($fields);
         $fields = static::parseFields($fields, static::getFilename($slug), $table);
+dump($fields['fperiodo']);
+        $queryBuilder = $this->getQueryBuilder();
+        $queryBuilder
+            ->select('u.*')
+            ->from(USER_TABLE, 'u')
+            ->where('u.edg020_id_periodo = :periodo')
+            ->andWhere('u.edg020_libro_escolaridad = :cod_alumno')
+            ->andWhere('u.edg020_fec_dia = :naci_dia_alumno')
+            ->andWhere('u.edg020_fec_mes = :naci_mes_alumno')
+            ->andWhere('u.edg020_libro_escolaridad = :cod_prueba')
+            ->setParameters(['periodo' => $periodo, 'cod_alumno' => $cod_alumno, 'naci_dia_alumno' => $naci_dia_alumno, 'naci_mes_alumno' => $naci_mes_alumno])/*
+            ->leftJoin('u', 'phonenumbers', 'p', 'u.id = p.user_id')*/;
+        if ($query = $queryBuilder->execute()) {
+            dump($queryBuilder->getSQL());
+            dump($query->fetch());
+        }
         dump($fields);
+
+        dump($_POST);
         /*$codigo = $fields['fcodalued'];
         $codigoOK = $this->select($table, '*', ["`$table.edg020_libro_escolaridad`" => "`$codigo`;"]);
         //$this->select($break_table);
